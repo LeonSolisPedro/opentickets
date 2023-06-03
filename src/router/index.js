dd0cee0ad528e992a1ace7ca7fc18be0f8bac040
Pedro León <pedro@wintercr.com>
2023-06-03 18:01:06 -0500

    Improving code readability

--- src/router/index.js
+++ src/router/index.js
@@ -3,64 +3,66 @@ import { redirectIndex, redirectRoles } from "./redirects"
 import DashboardView from "../views/Shared/DashboardView.vue"
 import EmptyView from "../views/Shared/EmptyView.vue"
 
+const routes = [
+  {
+    path: "/",
+    component: DashboardView,
+    children: [
+
+      //Administrador
+      {
+        path: "/tickets",
+        component: EmptyView,
+        children: [
+          {
+            path: "index",
+            component: () => import("@/views/Tickets/Index.vue"),
+          },
+        ]
+      },
+      {
+        path: "/computadoras",
+        component: EmptyView,
+        children: [
+          {
+            path: "index",
+            component: () => import("@/views/Computadoras/Index.vue"),
+          },
+        ]
+      },
+      {
+        path: "/empleados",
+        component: EmptyView,
+        children: [
+          {
+            path: "index",
+            component: () => import("@/views/Empleados/Index.vue"),
+          },
+        ]
+      },
+
+      //Error Pages
+      {
+        path: "/notfound",
+        component: () => import("@/views/Shared/Error/NotFound.vue"),
+      },
+      {
+        path: "/internalerror",
+        component: () => import("@/views/Shared/Error/InternalError.vue"),
+      },
+
+    ]
+  },
+  {
+    path: '/:catchAll(.*)', redirect: 'notfound'
+  }
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   linkActiveClass: "active",
   base: import.meta.env.BASE_URL,
-  routes: [
-    {
-      path: "/",
-      component: DashboardView,
-      children: [
-        
-        //Administrador
-        {
-          path: "/tickets",
-          component: EmptyView,
-          children: [
-            {
-              path: "/tickets/index",
-              component: () => import("@/views/Tickets/Index.vue"),
-            },
-          ]
-        },
-        {
-          path: "/computadoras",
-          component: EmptyView,
-          children: [
-            {
-              path: "/computadoras/index",
-              component: () => import("@/views/Computadoras/Index.vue"),
-            },
-          ]
-        },
-        {
-          path: "/empleados",
-          component: EmptyView,
-          children: [
-            {
-              path: "/empleados/index",
-              component: () => import("@/views/Empleados/Index.vue"),
-            },
-          ]
-        },
-        
-        //Error Pages
-        {
-          path: "/notfound",
-          component: () => import("@/views/Shared/Error/NotFound.vue"),
-        },
-        {
-          path: "/internalerror",
-          component: () => import("@/views/Shared/Error/InternalError.vue"),
-        },
-
-      ]
-    },
-    {
-      path: '/:catchAll(.*)', redirect:'notfound'
-    }
-  ]
+  routes
 });
 
 router.beforeEach((to, from, next) => redirectRoles(to, from, next))
